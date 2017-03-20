f5849e84f7bea029c2c7a9cfd2fa54be2e0a5f5a
dani5636 <dani5636@easv365.dk>
2017-03-20 11:43:59 +0100

    Commiting to work at home
    Noise

--- CA1AttendanceProgram-master/src/ca1attendanceprogram/BE/StudentLesson.java
+++ CA1AttendanceProgram-master/src/ca1attendanceprogram/BE/StudentLesson.java
@@ -11,5 +11,43 @@ package ca1attendanceprogram.BE;
  */
 public class StudentLesson
   {
+    Student student;
+    Lesson lesson;
+    String Attending;
+    private final static String ABSENT= "Absent";
+    private final static String ATTENDING = "Attending";
+    private final static String MERCY_REQUESTED ="Absent(Mercy Requested)";
+
+    public Student getStudent()
+      {
+        return student;
+      }
+
+    public void setStudent(Student student)
+      {
+        this.student = student;
+      }
+
+    public Lesson getLesson()
+      {
+        return lesson;
+      }
+
+    public void setLesson(Lesson lesson)
+      {
+        this.lesson = lesson;
+      }
+
+    public String getAttending()
+      {
+        return Attending;
+      }
+
+    public void setAttending(String Attending)
+      {
+        this.Attending = Attending;
+      }
     
+    
+   
   }
