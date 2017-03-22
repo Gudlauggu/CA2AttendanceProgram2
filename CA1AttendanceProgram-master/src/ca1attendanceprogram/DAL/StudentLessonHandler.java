9a8683d47e431ba3cacebb7b327c885ba8bbbd9b
PBLauge <peder.laugesen@gmail.com>
2017-03-22 09:11:52 +0100

    StudentLessonHandler added

---
+++ CA1AttendanceProgram-master/src/ca1attendanceprogram/DAL/StudentLessonHandler.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca1attendanceprogram.DAL;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Peder
+ */
+public class StudentLessonHandler
+{
+
+    SQLConnectionManager conManager;
+
+    public StudentLessonHandler()
+    {
+        conManager = new SQLConnectionManager();
+    }
+
+    public ArrayList<String> getAllStudentLessons()
+    {
+        try (Connection con = conManager.getConnection())
+        {
+            String query = "SELECT * FROM [Studentlesson]";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> studentLessons = new ArrayList<>();
+            while (rs.next())
+            {
+                String studentLessonString = "";
+                studentLessonString += rs.getString("lessonid") + " ";
+                studentLessonString += rs.getString("studentid") + " ";
+                studentLessonString += rs.getString("attending") + " ";
+
+                studentLessons.add(studentLessonString);
+            }
+            return studentLessons;
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle);
+            return null;
+        }
+    }
+
+    public ArrayList<String> getLessonId()
+    {
+        try (Connection con = conManager.getConnection())
+        {
+            String query = "SELECT * FROM [Studentlesson]";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> studentLessons = new ArrayList<>();
+            while (rs.next())
+            {
+                String studentLessonString = "";
+                studentLessonString += rs.getString("lessonid") + " ";
+
+                studentLessons.add(studentLessonString);
+            }
+            return studentLessons;
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle);
+            return null;
+        }
+
+    }
+
+    public ArrayList<String> getStudentId()
+    {
+        try (Connection con = conManager.getConnection())
+        {
+            String query = "SELECT * FROM [Studentlesson]";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> studentLessons = new ArrayList<>();
+            while (rs.next())
+            {
+                String studentLessonString = "";
+                studentLessonString += rs.getString("studentid") + " ";
+
+                studentLessons.add(studentLessonString);
+            }
+            return studentLessons;
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle);
+            return null;
+        }
+
+    }
+
+    public ArrayList<String> getAttending()
+    {
+        try (Connection con = conManager.getConnection())
+        {
+            String query = "SELECT * FROM [Studentlesson]";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> studentLessons = new ArrayList<>();
+            while (rs.next())
+            {
+                String studentLessonString = "";
+                studentLessonString += rs.getString("attending") + " ";
+
+                studentLessons.add(studentLessonString);
+            }
+            return studentLessons;
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle);
+            return null;
+        }
+
+    }
+}
